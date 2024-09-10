cd15559fa2237dbafe617e0d78a452c28985f87c
Robert151003 <148344123+Robert151003@users.noreply.github.com>
2024-09-10 11:21:47 +0100

    sidebar navbar

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+export const sidebarLinks=[
+    {
+        label:'Home',
+        route:'/',
+        imgUrl: '/icons/home.svg',
+    },
+    {
+        label:'Gaming',
+        route:'/Gaming',
+        imgUrl: '/icons/games.svg',
+    },
+    {
+        label:'Tv-Shows',
+        route:'/Tv-Shows',
+        imgUrl: '/icons/tv.svg',
+    },
+    {
+        label:'Movies',
+        route:'/Movies',
+        imgUrl: '/icons/movie.svg',
+    },
+    
+]
+    
