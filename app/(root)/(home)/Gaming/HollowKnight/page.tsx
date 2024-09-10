9e97f87e540470ca42330d23c58399133ef9cb3c
Robert151003 <148344123+Robert151003@users.noreply.github.com>
2024-09-10 15:15:17 +0100

    bug fix

--- app/(root)/(home)/Gaming/HollowKnight/page.tsx
+++ app/(root)/(home)/Gaming/HollowKnight/page.tsx
@@ -1,19 +1,17 @@
 'use client'
 
 import React from 'react';
-import { useTheme } from 'next-themes';
 
-const Page = () => {
-  const { theme } = useTheme()
+const page = () => {
   return (
     <section
       className={`flex flex-col gap-10 max-w-md ml-0 pl-4`}
     >
-      <h1 className='text-3xl font-bold '>
+      <h1 className='text-3xl font-bold'>
         Hollow Knight
       </h1>
     </section>
   )
 }
 
-export default Page
+export default page
