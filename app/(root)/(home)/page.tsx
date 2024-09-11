3f1e67b5f7bb1d3520daa8d484fcf97da59c9dab
Robert151003 <148344123+Robert151003@users.noreply.github.com>
2024-09-11 19:54:09 +0100

    bug fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const Home = () => {
-  
   return (
     <section
       className={`flex size-full flex-col gap-10`}
     >
-
       <h1 className='text-3xl font-bold '>
         Home
       </h1>
@@ -15,8 +13,7 @@ const Home = () => {
       <p>The point is just relax and navigate your way to your choice of time wasting??</p>
       <p>I update this blog regularly so hit that star up in your navigation bar to keep up to date since as you can see its not an ideal domain name.</p>
       <p>I will be keeping you up to date with the games that I am playing, giving walkthroughs and reviews as well as any new movies that i see and giving you my opinion on them.</p> 
-      <p>Furthermore i'll keep you up to date with any upcoming projects i am working on and any updates i plan to add to this blog post.</p>
-      
+      <p>Furthermore i&apos;ll keep you up to date with any upcoming projects i am working on and any updates i plan to add to this blog post.</p>
     </section>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Analytics />
           {children}
         </ThemeProvider>
       </body>
